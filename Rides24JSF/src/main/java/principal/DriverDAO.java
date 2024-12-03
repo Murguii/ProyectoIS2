bc86fd80fc83cd2d6b09e0820d7301d99dfb3378
Murguii <muurguii10@gmail.com>
2024-12-03 19:40:34 +0100

    base hibernate

---
+++ Rides24JSF/src/main/java/principal/DriverDAO.java
@@ -0,0 +1,15 @@
+package principal;
+
+public class DriverDAO {
+
+	public DriverDAO() {}
+	
+	/*Esquema de la transaccion
+	 * EntityManager em = JPAUtil.getEntityManager();
+ 		em.getTransaction().begin();
+ 		// OPERACIONES con la BD (que componen la transacción)
+ 		em.getTransaction().commit();
+	 */
+	
+	//métodos para gestionar el acceso a datos de driver
+}
