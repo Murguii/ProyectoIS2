679db306d1026be6b9260d28b5401a711efeb484
Dasilve <igordsl712@gmail.com>
2024-12-03 18:38:08 +0100

    Register y su bean

---
+++ Rides24JSF/src/main/java/modelo/bean/RegisterBean.java
@@ -0,0 +1,36 @@
+package modelo.bean;
+import java.io.Serializable;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+@Named("register")
+@SessionScoped
+
+public class RegisterBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+	private String password;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String doRegister() {
+		return "cambiar";
+	}
+	
+	public String close() {
+		return "close";
+	}
+
+}
